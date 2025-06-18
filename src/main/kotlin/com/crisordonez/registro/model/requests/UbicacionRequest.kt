d2a75b555ba161da2d8c4c7db3bc14ebfe6f2d70
JonnathanAguilar <jonnathan.aguilarl@ucuenca.edu.ec>
2025-06-17 22:04:33 -0500

    [DEL] Remove redundant mapping methods from UbicacionRequest and UbicacionResponse

--- src/main/kotlin/com/crisordonez/registro/model/requests/UbicacionRequest.kt
+++ src/main/kotlin/com/crisordonez/registro/model/requests/UbicacionRequest.kt
@@ -1,7 +1,4 @@
-package com.crisordonez.registro.model.dto
-
-import com.crisordonez.registro.model.entities.UbicacionEntity
-import com.crisordonez.registro.model.enums.EstablecimientoEnum
+package com.crisordonez.registro.model.requests
 
 data class UbicacionRequest(
     val nombre: String,
@@ -12,17 +9,4 @@ data class UbicacionRequest(
     val latitud: Double,
     val longitud: Double,
     val establecimiento: String // lo que llega en el JSON
-) {
-    fun toEntity(): UbicacionEntity {
-        return UbicacionEntity(
-            nombre = nombre,
-            telefono = telefono,
-            direccion = direccion,
-            horario = horario,
-            sitioWeb = sitioWeb,
-            latitud = latitud,
-            longitud = longitud,
-            establecimiento = EstablecimientoEnum.valueOf(establecimiento)
-        )
-    }
-}
+)
--- src/main/kotlin/com/crisordonez/registro/model/responses/UbicacionResponse.kt
+++ src/main/kotlin/com/crisordonez/registro/model/responses/UbicacionResponse.kt
@@ -1,6 +1,5 @@
 package com.crisordonez.registro.model.responses
 
-import com.crisordonez.registro.model.entities.UbicacionEntity
 import java.util.UUID
 
 data class UbicacionResponse(
@@ -13,20 +12,4 @@ data class UbicacionResponse(
     val latitud: Double,
     val longitud: Double,
     val establecimiento: String
-) {
-    companion object {
-        fun fromEntity(entity: UbicacionEntity): UbicacionResponse {
-            return UbicacionResponse(
-                publicId = entity.publicId,
-                nombre = entity.nombre,
-                telefono = entity.telefono,
-                direccion = entity.direccion,
-                horario = entity.horario,
-                sitioWeb = entity.sitioWeb,
-                latitud = entity.latitud,
-                longitud = entity.longitud,
-                establecimiento = entity.establecimiento.toString()
-            )
-        }
-    }
-}
+)
