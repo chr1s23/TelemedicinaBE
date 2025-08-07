702f465c8c3055e5d8d6c3b06f51f458412c44d7
JonnathanAguilar <jonnathan.aguilarl@ucuenca.edu.ec>
2025-08-07 13:54:39 -0500

    [CHORE] Changes in permissions

--- src/main/kotlin/com/crisordonez/registro/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/crisordonez/registro/configuration/SecurityConfig.kt
@@ -88,15 +88,15 @@ class SecurityConfig {
                 registry.requestMatchers(HttpMethod.GET,  "/prueba/medico/prefixes").permitAll()
 
                 // 13) CRUD de ubicaciones
-                //registry.requestMatchers(HttpMethod.GET, "/api/ubicaciones/**").permitAll()
-                //registry.requestMatchers(HttpMethod.POST, "/api/ubicaciones/**").permitAll()
-                //registry.requestMatchers(HttpMethod.PUT, "/api/ubicaciones/**").permitAll()
-                //registry.requestMatchers(HttpMethod.DELETE, "/api/ubicaciones/**").permitAll()
-                registry.requestMatchers("/api/ubicaciones/**").hasRole("ADMIN") // Produccion
+                registry.requestMatchers(HttpMethod.GET, "/api/ubicaciones/**").permitAll()
+                registry.requestMatchers(HttpMethod.POST, "/api/ubicaciones/**").permitAll()
+                registry.requestMatchers(HttpMethod.PUT, "/api/ubicaciones/**").permitAll()
+                registry.requestMatchers(HttpMethod.DELETE, "/api/ubicaciones/**").permitAll()
+                //registry.requestMatchers("/api/ubicaciones/**").hasRole("ADMIN") // Produccion
 
                 // 14) Listar Estados de Dispositivos
-                //registry.requestMatchers(HttpMethod.GET, "/api/estado-dispositivos/**").permitAll() // Desarrollo
-                registry.requestMatchers(HttpMethod.GET, "/api/estado-dispositivos/**").hasRole("ADMIN") // Produccion
+                registry.requestMatchers(HttpMethod.GET, "/api/estado-dispositivos/**").permitAll() // Desarrollo
+                //registry.requestMatchers(HttpMethod.GET, "/api/estado-dispositivos/**").hasRole("ADMIN") // Produccion
 
 
                 // APP MOVIL
