38581a2f3253d146bbc992eb3677f7787f1b4d94
Emily Romero <romero.emily.225@gmail.com>
2025-08-12 19:16:53 -0500

    [FEAT] Update of device status reports by Statu and Date

--- src/main/resources/static/web/flutter_service_worker.js
+++ src/main/resources/static/web/flutter_service_worker.js
@@ -30,14 +30,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "50469bd78c8ff849fa94f0226a9f18e3
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "032ad3448dd39c0c328ed62b9684a42a",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "771a683b5e1f8dadd7f2953609745d98",
+"flutter_bootstrap.js": "cb4ce6e37f3e3a678d69fece47234424",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "20041864f990697b0e66d803ddf8780b",
 "/": "20041864f990697b0e66d803ddf8780b",
-"main.dart.js": "9eab882fa8c5416a1341f0a55547c973",
+"main.dart.js": "8111a3a001f791329e9154cab4b16bd1",
 "manifest.json": "223e7c6ba156d237acba62d16c7475d6",
 "version.json": "297d38b84aabe97688a7ac62bb1e1e2e"};
 // The application shell files that are downloaded before a service worker can
